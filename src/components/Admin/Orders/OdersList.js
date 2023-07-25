f85481af5a906762ca9f9f472752f0e58ad115b0
pavankumar130 <20bcs130@iiitdmj.ac.in>
2023-07-25 18:06:34 +0530

    modified:   src/components/Admin/Orders/OdersList.js

--- src/components/Admin/Orders/OdersList.js
+++ src/components/Admin/Orders/OdersList.js
@@ -1,35 +1,35 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import { fetchOrdersAction } from "../../../redux/slices/orders/ordersSlices";
-import ErrorMsg from "../../ErrorMsg/ErrorMsg";
-import LoadingComponent from "../../LoadingComp/LoadingComponent";
-import NoDataFound from "../../NoDataFound/NoDataFound";
-import SuccessMsg from "../../SuccessMsg/SuccessMsg";
-import OrdersStats from "./OrdersStatistics";
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { fetchOrdersAction } from '../../../redux/slices/orders/ordersSlices'
+import ErrorMsg from '../../ErrorMsg/ErrorMsg'
+import LoadingComponent from '../../LoadingComp/LoadingComponent'
+import NoDataFound from '../../NoDataFound/NoDataFound'
+import SuccessMsg from '../../SuccessMsg/SuccessMsg'
+import OrdersStats from './OrdersStatistics'
 
 const people = [
   {
-    name: "Lindsay Walton",
-    title: "Front-end Developer",
-    email: "lindsay.walton@example.com",
-    role: "Member",
+    name: 'Lindsay Walton',
+    title: 'Front-end Developer',
+    email: 'lindsay.walton@example.com',
+    role: 'Member',
   },
   // More people...
-];
+]
 
 export default function OrdersList() {
   //dispatch
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(fetchOrdersAction());
-  }, [dispatch]);
+    dispatch(fetchOrdersAction())
+  }, [dispatch])
   //get data from store
   const {
     error,
     loading,
     orders: { orders },
-  } = useSelector((state) => state?.orders);
+  } = useSelector((state) => state?.orders)
 
   return (
     <>
@@ -48,33 +48,33 @@ export default function OrdersList() {
               <tr>
                 <th
                   scope="col"
-                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">
+                  className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
+                >
                   Order ID
                 </th>
                 <th
                   scope="col"
-                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell"
+                >
                   Payment Status
                 </th>
                 <th
                   scope="col"
-                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">
+                  className="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell"
+                >
                   Oder Date
                 </th>
                 <th
                   scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                  Delivery Date
-                </th>
-                <th
-                  scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
                   Status
                 </th>
 
                 <th
                   scope="col"
-                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                  className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                >
                   Total
                 </th>
                 {/* <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
@@ -90,41 +90,69 @@ export default function OrdersList() {
               <tbody className="divide-y divide-gray-200 bg-white">
                 {orders?.map((order) => (
                   <tr key={order._id}>
-                    <td className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6">
+                    <td
+                      className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6"
+                      style={{ fontWeight: 'bold' }}
+                    >
                       {order.orderNumber}
                     </td>
                     <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                      {order.paymentStatus === "Not paid" ? (
-                        <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-600 text-gray-300">
+                      {order.paymentStatus === 'Not paid' ? (
+                        <span
+                          style={{
+                            color: 'red',
+                            fontWeight: 'bold',
+                            textTransform: 'capitalize',
+                          }}
+                        >
                           {order.paymentStatus}
                         </span>
                       ) : (
-                        order.paymentStatus
+                        <span
+                          style={{
+                            color: 'green',
+                            fontWeight: 'bold',
+                            textTransform: 'capitalize',
+                          }}
+                        >
+                          {order.paymentStatus}
+                        </span>
                       )}
                     </td>
-                    <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
+                    <td
+                      className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell"
+                      style={{ fontWeight: 'bold' }}
+                    >
                       {new Date(order?.createdAt).toLocaleDateString()}
                     </td>
-                    <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
-                      Unknown
-                    </td>
-                    <td className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">
+                    <td
+                      className="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell"
+                      style={{
+                        textTransform: 'capitalize',
+                        fontWeight: 'bold',
+                      }}
+                    >
                       {order?.status}
                     </td>
-                    <td className="px-3 py-4 text-sm text-gray-500">
+                    <td
+                      className="px-3 py-4 text-sm text-gray-500"
+                      style={{ fontWeight: 'bold' }}
+                    >
                       {order?.totalPrice}
                     </td>
                     <td className="py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                      {order?.paymentStatus === "Not paid" ? (
+                      {order?.paymentStatus === 'Not paid' ? (
                         <Link
-                          style={{ cursor: "not-allowed" }}
-                          className="text-gray-300">
+                          style={{ cursor: 'not-allowed' }}
+                          className="text-gray-300"
+                        >
                           Edit
                         </Link>
                       ) : (
                         <Link
                           to={`/admin/orders/${order?._id}`}
-                          className="text-indigo-600 hover:text-indigo-900">
+                          className="text-orange-600 hover:text-green-900"
+                        >
                           Edit
                         </Link>
                       )}
@@ -137,5 +165,5 @@ export default function OrdersList() {
         </div>
       </div>
     </>
-  );
+  )
 }
