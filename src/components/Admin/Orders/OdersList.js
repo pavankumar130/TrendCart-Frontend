ce8eeb970467ea68df85ff331b1463b04ae87dbf
pavankumar130 <20bcs130@iiitdmj.ac.in>
2023-08-08 16:51:01 +0530

    modified:   src/components/Admin/Orders/OdersList.js

--- src/components/Admin/Orders/OdersList.js
+++ src/components/Admin/Orders/OdersList.js
@@ -94,7 +94,7 @@ export default function OrdersList() {
                       className="w-full max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6"
                       style={{ fontWeight: 'bold' }}
                     >
-                      {order.orderNumber}
+                      {order._id}
                     </td>
                     <td className="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">
                       {order.paymentStatus === 'Not paid' ? (
